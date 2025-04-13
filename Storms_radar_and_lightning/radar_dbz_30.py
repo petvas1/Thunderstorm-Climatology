ff7f58e2c3e47fd29d83f1ccdefcc3f361fa3805
petvas1 <74054205+petvas1@users.noreply.github.com>
2025-04-13 23:18:01 +0200

    Update radar_dbz_30.py

--- Storms_radar_and_lightning/radar_dbz_30.py
+++ Storms_radar_and_lightning/radar_dbz_30.py
@@ -65,8 +65,7 @@ def process():
         process_image(rad)
 
 
-    # mapa_sr_kraje = gpd.read_file("/mnt/Qnas/Vas/Mapy_krajin/mapa_sr_kraje.csv")
-    mapa_sr_kraje = gpd.read_file("C:\\Users\\petva\\Desktop\\FMFI UK\\3. rocnik\\bakalarka\\Mapy krajin\\mapa_sr_kraje.csv")
+    mapa_sr_kraje = gpd.read_file("mapa_sr_kraje.csv")
     fig, ax = plt.subplots()
     X, Y = np.meshgrid(np.linspace(x_0, x_0 + (x2 - x1) * pixel_x, x2 - x1),
                        np.linspace(y_0, y_0 - (y2 - y1) * pixel_y, y2 - y1))
